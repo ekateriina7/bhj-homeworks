0068eade8cf4b8ba9ad0eb681913e598e58642c9
katya gavriuk <0303gavkat@gmail.com>
2019-04-25 22:42:01 +0300

    changes in 1-2 tasks

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,16 @@
-function navigateMenu(){
-    let menuLink = Array.from(document.querySelectorAll('ul li a.menu__link'));
-    let menu_sub = Array.from(document.querySelectorAll('ul.menu_sub'));
-    let about = menuLink[1];
-    let services = menuLink[5];
-    
-    about.onclick = function() {
-       menu_sub[0].className+= ' menu_active';
-       menu_sub[1].className = 'menu menu_sub';
-       return false;
-    }
+const links = Array.from(document.querySelectorAll('.menu__link'));
 
-    services.onclick = function() {
-        menu_sub[1].className+= ' menu_active';
-        menu_sub[0].className = 'menu menu_sub';
-        return false;
+for (let link of links) {
+    link.onclick = function () {
+        const parent = link.parentElement;
+        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+        } else {
+            parent.querySelector('.menu_sub').className = 'menu menu_sub'
+        }
+
+        if (link.closest('.menu_main')) {
+            return false
+        }
     }
 }
-
-navigateMenu();
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,14 +3,15 @@ let modal__close = document.querySelectorAll('div.modal__close');
 let show_success = document.querySelector('.show-success');
 let modal_success = document.querySelector('#modal_success');
 modal_main.className = 'modal modal_active';
+
 modal__close[0].onclick = function(){
-    modal_main.className = 'modal';
+    modal_main.className = 'modal'
 }
 show_success.onclick = function(){
+    modal_main.className = 'modal'
     modal_success.className = 'modal modal_active';
 }
 modal__close[1].onclick = function(){
     modal_success.className = 'modal';
     modal_main.className = 'modal';
 }
-
